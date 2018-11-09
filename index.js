bd8dc723f82ea20ae377d54de09644a7e1c5d27c
Gabe Abrams <gabeabrams@gmail.com>
2018-11-09 11:21:51 -0500

    Added manual login process, renamed options => config

--- index.js
+++ index.js
@@ -25,15 +25,17 @@ const sendRequest = require('./sendRequest.js');
  *   storage of refresh tokens, exclude parameter to use memory token store,
  *   or include a custom token store of form { get(key), set(key, val) } where
  *   both functions return promises
+ * @param {function} [onManualLogin] - a function to call with params (req, res)
+ *   after req.logInManually is called and finishes manually logging in
  * @return {string} authorizePath that is being served
  */
-module.exports = (options) => {
-  // Check if required options are included
+module.exports = (config) => {
+  // Check if required config are included
   if (
-    !options
-    || !options.app
-    || !options.canvasHost
-    || !options.developerCredentials
+    !config
+    || !config.app
+    || !config.canvasHost
+    || !config.developerCredentials
   ) {
     throw new CACCLError({
       message: 'Token manager initialized improperly: at least one required option was not included. We require app, canvasHost, developerCredentials',
@@ -42,35 +44,35 @@ module.exports = (options) => {
   }
 
   // Initialize authorizePath
-  const authorizePath = options.authorizePath || '/authorize';
+  const authorizePath = config.authorizePath || '/authorize';
 
   // Initialize autoRefreshRoutes
-  const autoRefreshRoutes = options.autoRefreshRoutes || ['*'];
+  const autoRefreshRoutes = config.autoRefreshRoutes || ['*'];
 
   // Initialize the default authorized redirect path
   const defaultAuthorizedRedirect = (
-    options.defaultAuthorizedRedirect || authorizePath + '/done'
+    config.defaultAuthorizedRedirect || authorizePath + '/done'
   );
 
   // Initialize token store
   let tokenStore;
-  if (options.tokenStore === null) {
+  if (config.tokenStore === null) {
     // Null specifically included, do not use a token store
     tokenStore = null;
-  } else if (options.tokenStore === undefined) {
+  } else if (config.tokenStore === undefined) {
     // No token store included, use memory store
     tokenStore = new MemoryTokenStore();
   } else {
     // Custom token store included
     // Validate its functionality
-    if (!options.tokenStore.get || !options.tokenStore.set) {
+    if (!config.tokenStore.get || !config.tokenStore.set) {
       throw new CACCLError({
         message: 'Token manager initialized improperly: your custom token store is invalid. It must include a get and a set function.',
         code: errorCodes.tokenStoreInvalidWrongFunctions,
       });
     }
     // Custom token store valid
-    ({ tokenStore } = options.tokenStore);
+    ({ tokenStore } = config.tokenStore);
   }
 
   // Create refresh function
@@ -84,13 +86,13 @@ module.exports = (options) => {
       return Promise.resolve(false);
     }
     return sendRequest({
-      host: options.canvasHost,
+      host: config.canvasHost,
       path: '/login/oauth2/token',
       method: 'POST',
       params: {
         grant_type: 'refresh_token',
-        client_id: options.developerCredentials.client_id,
-        client_secret: options.developerCredentials.client_secret,
+        client_id: config.developerCredentials.client_id,
+        client_secret: config.developerCredentials.client_secret,
         refresh_token: refreshToken,
       },
     })
@@ -121,12 +123,45 @@ module.exports = (options) => {
   };
 
   /*------------------------------------------------------------------------*/
+  /*                      Manual Authorization Process                      */
+  /*------------------------------------------------------------------------*/
+
+  config.app.use((req, res, next) => {
+    req.logInManually = (accessToken, refreshToken, expiry) => {
+      // Save in session
+      req.session.accessToken = accessToken;
+      req.session.accessTokenExpiry = expiry;
+      req.session.refreshToken = refreshToken;
+
+      // Send callback
+      if (config.onManualLogin) {
+        config.onManualLogin(req, res);
+      }
+
+      // Save session
+      return new Promise((resolve, reject) => {
+        req.session.save((err) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve({
+            accessToken,
+            refreshToken,
+          });
+        });
+      });
+    };
+
+    next();
+  });
+
+  /*------------------------------------------------------------------------*/
   /*                          Authorization Process                         */
   /*------------------------------------------------------------------------*/
 
   // Step 1: Try to refresh, if not possible, redirect to authorization screen
 
-  options.app.all(authorizePath, (req, res) => {
+  config.app.all(authorizePath, (req, res) => {
     // Extract the next path
     const nextPath = (
       req.query.next
@@ -161,12 +196,12 @@ module.exports = (options) => {
           return res.redirect(nextPath);
         }
         // Refresh failed. Redirect to start authorization process
-        return res.redirect('https://' + options.canvasHost + '/login/oauth2/auth?client_id=' + options.developerCredentials.client_id + '&response_type=code&redirect_uri=https://' + req.headers.host + authorizePath + '/responded?state=' + nextPath);
+        return res.redirect('https://' + config.canvasHost + '/login/oauth2/auth?client_id=' + config.developerCredentials.client_id + '&response_type=code&redirect_uri=https://' + req.headers.host + authorizePath + '/responded?state=' + nextPath);
       });
   });
 
   // Step 2: Receive code or denial
-  options.app.get(authorizePath + '/responded', (req, res) => {
+  config.app.get(authorizePath + '/responded', (req, res) => {
     // Make sure we have the correct response from Canvas
     if (!req.query.state) {
       // Authorization failed
@@ -195,12 +230,12 @@ module.exports = (options) => {
 
     // Attempt to trade access token for actual access token
     sendRequest({
-      host: options.canvasHost,
+      host: config.canvasHost,
       path: '/login/oauth2/token',
       params: {
         code,
-        client_id: options.developerCredentials.client_id,
-        client_secret: options.developerCredentials.client_secret,
+        client_id: config.developerCredentials.client_id,
+        client_secret: config.developerCredentials.client_secret,
         redirect_uri: 'https://' + req.headers.host + nextPath,
       },
     })
@@ -214,27 +249,18 @@ module.exports = (options) => {
         const accessTokenExpiry = new Date().getTime() + 3540000;
 
         // Save in session
-        req.session.accessToken = accessToken;
-        req.session.accessTokenExpiry = accessTokenExpiry;
-        req.session.refreshToken = refreshToken;
-        return new Promise((resolve, reject) => {
-          req.session.save((err) => {
-            if (err) {
-              return reject(err);
-            }
-            return resolve({
-              accessToken,
-              refreshToken,
-            });
-          });
-        });
+        return req.logInManually(
+          accessToken,
+          refreshToken,
+          accessTokenExpiry
+        );
       })
       .then((tokens) => {
         // Store in token store (if applicable)
         if (tokenStore) {
           // Lookup current user's Canvas ID, to use as a key in the store
           return sendRequest({
-            host: options.canvasHost,
+            host: config.canvasHost,
             path: '/api/v1/users/self/profile',
             params: {
               access_token: tokens.accessToken,
@@ -264,7 +290,7 @@ module.exports = (options) => {
 
   autoRefreshRoutes.forEach((autoRefreshRoute) => {
     // Add middleware to automatically refresh the access token upon expiry
-    options.app.use(autoRefreshRoute, (req, res, next) => {
+    config.app.use(autoRefreshRoute, (req, res, next) => {
       // Check if we have a token
       if (
         !req.session
