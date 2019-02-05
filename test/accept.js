3a9ff22be8e626522d4238484c8c6ad13c4a7348
Gabe Abrams <gabeabrams@gmail.com>
2019-02-05 15:17:15 -0500

    Updated to use new dce-selenium structure

---
+++ test/accept.js
@@ -0,0 +1,7 @@
+const createAppCanvasPair = require('./helpers/createAppCanvasPair');
+
+createAppCanvasPair();
+
+console.log('Visit the following urls in all browsers you plan to test in:');
+console.log('https://localhost:8088/verifycert');
+console.log('https://localhost:8089/verifycert');
--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
-const Mocha = require('mocha');
 const fs = require('fs');
 const path = require('path');
+const runSelenium = require('dce-selenium/run');
 
 const createAppCanvasPair = require('./helpers/createAppCanvasPair');
 
@@ -10,15 +10,12 @@ const createAppCanvasPair = require('./helpers/createAppCanvasPair');
 /*                               Mocha Tests                              */
 /*------------------------------------------------------------------------*/
 
-let numFailures = 0;
-let backupDescribe;
 const runTests = async (folderName) => {
   // Print
-  const folderNameParts = folderName.split('/');
-  console.log(`Running tests in ${folderNameParts[folderNameParts.length - 1]}`);
+  console.log(`\n\nRunning tests in ${folderName}\n`);
 
   // Read configuration
-  const configFilename = path.join(folderName, 'config.js');
+  const configFilename = path.join(__dirname, 'selenium', folderName, 'config.js');
   /* eslint-disable global-require */
   /* eslint-disable import/no-dynamic-require */
   const config = require(configFilename);
@@ -26,36 +23,9 @@ const runTests = async (folderName) => {
   // Start app and canvas simulator
   await createAppCanvasPair(config);
 
-  // Start Mocha tests
-  // Instantiate a Mocha instance.
-  const mocha = new Mocha({
-    reporter: 'spec',
-  });
-
-  // Add each .js file to the mocha instance
-  fs.readdirSync(folderName).filter((file) => {
-    // Skip config.js
-    if (file === 'config.js') {
-      return false;
-    }
-    // Only keep the .js files
-    return (file.substr(-3) === '.js');
-  })
-    .forEach((file) => {
-      mocha.addFile(
-        path.join(folderName, file)
-      );
-    });
-
-  // Run the tests
-  await new Promise((resolve) => {
-    mocha.run((failures) => {
-      numFailures += failures;
-      if (!backupDescribe) {
-        backupDescribe = global.describe;
-      }
-      resolve();
-    });
+  // Start tests
+  await runSelenium({
+    subfolder: folderName,
   });
 };
 
@@ -65,23 +35,15 @@ const isDirectory = (location) => {
 };
 const listDirectories = (location) => {
   return fs.readdirSync(location)
-    .map((name) => {
-      return path.join(location, name);
-    })
-    .filter(isDirectory);
+    .filter((folder) => {
+      return isDirectory(path.join(location, folder));
+    });
 };
-const dirs = listDirectories(path.join(__dirname, 'selenium-tests'));
+const dirs = listDirectories(path.join(__dirname, 'selenium'));
 const runAllTests = async () => {
   /* eslint-disable no-await-in-loop */
   for (let i = 0; i < dirs.length; i++) {
     await runTests(dirs[i]);
-  }
-  // Print out number of failures
-  if (numFailures === 0) {
-    console.log('\nAll tests passed!');
-  } else {
-    console.log('\n' + '!'.repeat(10) + ' Notice: ' + '!'.repeat(10));
-    console.log('\nSome tests failed!\nScroll back up through the many Mocha reports (each folder in test/selenium-tests runs separately) and find the errors.\n');
   }
   process.exit(0);
 };
